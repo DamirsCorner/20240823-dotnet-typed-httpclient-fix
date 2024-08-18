472d10f34e1ce997e058f4e8738020d1f8e8a2cd
Damir Arh <damir.arh@gmail.com>
2024-08-18 11:50:09 +0200

    Fix HttpClient registration

--- TypedHttpClientFix/Program.cs
+++ TypedHttpClientFix/Program.cs
@@ -11,8 +11,6 @@ builder.Services.AddHttpClient<SampleService>(client =>
     );
 });
 
-builder.Services.AddScoped<SampleService>();
-
 builder.Services.AddControllers();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
